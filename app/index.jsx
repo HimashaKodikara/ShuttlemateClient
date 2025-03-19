9ff03ded0b37ea944b536b527cf0e4e95d70cfd4
19840769 <kodikarah.21@uom.lk>
2025-03-19 20:20:19 +0530

    (changes)

---
+++ app/index.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {Link} from 'expo-router';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Header}>Shuttlemate</Text>
+      <StatusBar style="auto" />
+        <Link href="/profile" style={{color:'blue'}}>Go to</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Header:{
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+});
